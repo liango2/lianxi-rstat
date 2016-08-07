992343376d814debc119a3ffb3ad15ac242e7f92
liango2 <540625404@qq.com>
2016-08-07 18:43:23 +0800

    2016-08-07 18:43:22

--- myDataFrame2InserSql.R
+++ myDataFrame2InserSql.R
@@ -1,29 +1,25 @@
-fruits <-data.frame(id=1:5,name=c("苹果","香蕉","梨子","玉米","西瓜"),price=c(8.8,4.98,7.8,6,2.1),status=c("无","打折","无","售罄","批发"))  
-
-
-insertSql <- function(fruits){
+insertSql <- function(myDataFrame, myTableName){
   #  INSERT INTO test (
   #   id, name, price, status
   #  ) VALUES 
   #  (id, name, price, status)
   # ,(id, name, price, status)
   
-  myTableName <- "test" 
   line1 <- paste("INSERT INTO", myTableName, "(")
   
-  cols <- colnames(fruits)
+  cols <- colnames(myDataFrame)
   line2 <- paste(cols, collapse=",")
   
   line3 <- paste(") VALUES")
   
-  #line4_0 <- sprintf("('%s', '%s', '%s', '%s')", fruits[1:5, 1], fruits[1:5, 2], fruits[1:5, 3], fruits[1:5, 4])
-  nr <- nrow(fruits)
-  nc <- ncol(fruits)
+  #line4_0 <- sprintf("('%s', '%s', '%s', '%s')", myDataFrame[2:5, 1], myDataFrame[1:5, 2], myDataFrame[1:5, 3], myDataFrame[1:5, 4])
+  nr <- nrow(myDataFrame)
+  nc <- ncol(myDataFrame)
   
-  line4_1 <- sprintf("('%s'", fruits[1:nr, 1])
+  line4_1 <- sprintf("('%s'", myDataFrame[1:nr, 1])
   
   for(i in 2:nc) {
-      line4_1 <- sprintf(paste0(line4_1, ",'%s'"), fruits[1:nr, i]) 
+      line4_1 <- sprintf(paste0(line4_1, ",'%s'"), myDataFrame[1:nr, i]) 
   }
   
   line4 <- paste0(line4_1,  ")", collapse = ",")
@@ -33,5 +29,6 @@ insertSql <- function(fruits){
   return(lines)
 }
 
-strSql <- insertSql(fruits)
+myDataFrame <-data.frame(id=1:5,name=c("苹果","香蕉","梨子","玉米","西瓜"),price=c(8.8,4.98,7.8,6,2.1),status=c("无","打折","无","售罄","批发"))  
+strSql <- insertSql(myDataFrame, "test")
 print(strSql)
